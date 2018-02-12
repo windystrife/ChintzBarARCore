5ce91c4d910607615c600d1532ccd24638c7303b
Tung Thanh Nguyen <tungnt.rec@gmail.com>
2018-02-13 01:24:05 +0700

    ChintzBar 1.7
    ChintzBar 1.7

---
+++ Source/ChintzBarAR/Public/ARPlaneActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ARPlaneActor.generated.h"
+
+UCLASS()
+class CHINTZBARAR_API AARPlaneActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AARPlaneActor();
+
+	/* The procedural mesh component */
+	UPROPERTY(Category = GoogleARCorePlaneActor, EditAnywhere, BlueprintReadWrite)
+		class UProceduralMeshComponent* PlanePolygonMeshComponent;
+
+	/** When set to true, the actor will remove the ARAnchor object from the current tracking session when the Actor gets destroyed.*/
+	UPROPERTY(Category = GoogleARCorePlaneActor, BlueprintReadWrite)
+		class UGoogleARCorePlane* ARCorePlaneObject = nullptr;
+
+	/** The feathering distance for the polygon edge. Default to 10 cm*/
+	UPROPERTY(Category = GoogleARCorePlaneActor, EditAnywhere, BlueprintReadWrite)
+		float EdgeFeatheringDistance = 10.0f;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void UpdatePlanePolygonMesh();
+};
